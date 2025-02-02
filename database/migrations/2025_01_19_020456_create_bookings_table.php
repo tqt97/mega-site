bc151117c57763b7788dcbbfafaa5d7ad0f6e11c
tqt97 <kutuanonline199@gmail.com>
2025-02-02 17:33:53 +0700

    fix PR review

--- database/migrations/2025_01_19_020456_create_bookings_table.php
+++ database/migrations/2025_01_19_020456_create_bookings_table.php
@@ -22,10 +22,6 @@ return new class extends Migration
             $table->decimal('total_price', 10, 2);
             $table->timestamps();
 
-            // Index the check_in and check_out columns
-            $table->index('check_in');
-            $table->index('check_out');
-
             // Index the check_in and check_out columns together
             $table->index(['check_in', 'check_out']);
         });
